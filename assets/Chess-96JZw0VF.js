8e9b78cc59bbd17f483828f56df0b3441b1f7652
bondzioyo <bondzioyo@>
2024-04-04 14:15:36 +0200

    Updates

---
+++ assets/Chess-96JZw0VF.js
@@ -0,0 +1,1 @@
+import{a}from"./Board-LXJmwqIA.js";import{o as e,c as o,f as s}from"./index-R8uLU31m.js";const t={class:"container mx-auto grid gap-[50px] md:gap-[80px]"},p={__name:"Chess",setup(c){return(r,n)=>(e(),o("main",t,[s(a)]))}};export{p as default};
--- assets/Chess-cgBuzN5b.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./Board-DP68DEWv.js";import{o as e,c as o,f as s}from"./index-sbHppDVR.js";const t={class:"container mx-auto grid gap-[50px] md:gap-[80px]"},p={__name:"Chess",setup(c){return(r,n)=>(e(),o("main",t,[s(a)]))}};export{p as default};
