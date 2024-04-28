50282b0bc75dd6600d225597534674c172e40205
bondzioyo <bondzioyo@>
2024-04-28 21:43:40 +0200

    Updates

--- assets/Chess-U-0UFC_U.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./Board-00n5IUAr.js";import{o as e,c as o,f as s}from"./index-AWkBPNd8.js";const t={class:"container mx-auto grid gap-[50px] md:gap-[80px]"},p={__name:"Chess",setup(c){return(r,n)=>(e(),o("main",t,[s(a)]))}};export{p as default};
