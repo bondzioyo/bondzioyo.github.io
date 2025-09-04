118bb9b83471ab202c3427accf843ceb8509f376
Wojciech Swiatek <wojciechswiatek@SC-FY236M090Y0.local>
2025-09-04 17:53:39 +0200

    Updates

---
+++ assets/Chess-7NZDrBO5.js
@@ -0,0 +1,1 @@
+import{a}from"./Board-rt0RNaCj.js";import{o as e,c as o,h as s}from"./index--jTY5VqC.js";const t={class:"container mx-auto grid gap-[50px] md:gap-[80px]"},p={__name:"Chess",setup(c){return(r,n)=>(e(),o("main",t,[s(a)]))}};export{p as default};
--- assets/Chess-Im9Yg2hP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./Board-4WNN3ec5.js";import{o as e,c as o,f as s}from"./index-4ThQSH-4.js";const t={class:"container mx-auto grid gap-[50px] md:gap-[80px]"},p={__name:"Chess",setup(c){return(r,n)=>(e(),o("main",t,[s(a)]))}};export{p as default};
